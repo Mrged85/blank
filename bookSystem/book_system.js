b6f307fe7140a46ced8a6d57eecb5dcf683071aa
Mrged85 <goncalodamasramos@gmail.com>
2024-09-17 07:53:58 -0400

    Speed Analysis

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -1,24 +1,26 @@
 let books = [];
-  function addBook() {
-            const bookName = document.getElementById('bookName').value;
-            const authorName = document.getElementById('authorName').value;
-            const bookDescription = document.getElementById('bookDescription').value;
-            const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
-            if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
-                const book = {
-                    name: bookName,
-                    authorName: authorName,
-                    bookDescription: bookDescription,
-                    pagesNumber: pagesNumber
-                };
-                books.push(book);
-                showbooks();
-                clearInputs();
-            } else {
-                alert('Please fill in all fields correctly.');
-            }
-        }
-        function showbooks() {
+
+function addBook() {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+    if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+        const book = {
+            name: bookName,
+            authorName: authorName,
+            bookDescription: bookDescription,
+            pagesNumber: pagesNumber
+        };
+        books.push(book);
+        showbooks();
+        clearInputs();
+    } else {
+        alert('Please fill in all fields correctly.');
+    }
+}
+
+function showbooks() {
     const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
@@ -27,9 +29,10 @@ let books = [];
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
- function clearInputs() {
-            document.getElementById('bookName').value = '';
-            document.getElementById('authorName').value = '';
-            document.getElementById('bookDescription').value = '';
-            document.getElementById('pagesNumber').value = '';
- }
+
+function clearInputs() {
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
